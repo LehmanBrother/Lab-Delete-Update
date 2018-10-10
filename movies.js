cdeab8f4e7a127f9bce6dba0e8e96e7b8ab2e636
LehmanBrother <chrislehman1991@gmail.com>
2018-10-10 11:40:33 -0500

    Movies array

---
+++ movies.js
@@ -0,0 +1,32 @@
+const movies = [
+	{
+		name: "Prometheus",
+		genre: "Sci-Fi",
+		year: 2012
+	},
+	{
+		name: "Interstellar",
+		genre: "Sci-Fi",
+		year: 2014
+	},
+	{
+		name: "The Dark Knight",
+		genre: "Superhero",
+		year: 2008
+	},
+	{
+		name: "In Bruges",
+		genre: "Drama",
+		year: 2008
+	},
+	{
+		name: "The Social Network",
+		genre: "Drama",
+		year: 2010
+	},
+	{
+		name: "Singin' in the Rain",
+		genre: "Musical",
+		year: 1952
+	},
+]
