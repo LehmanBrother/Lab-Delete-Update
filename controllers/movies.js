98ebf4521f5eb75c874a6ffb33882c52915b95c1
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-10 12:08:40 -0500

    Edit page now live

--- controllers/movies.js
+++ controllers/movies.js
@@ -9,11 +9,29 @@ router.get('/', (req,res) => {
 		movies: Movies
 	})
 })
+//post route
+router.post('/', (req, res) => {
+	Movies.push(req.body)
+	res.redirect('/movies')
+});
 
 //delete route
 router.delete('/:index', (req,res) => {
 	Movies.splice(req.params.index,1);
 	res.redirect('/movies');
+})
+
+//edit route
+router.get('/:id/edit', (req, res) => {
+	res.render('edit.ejs', {
+		movie: Movies[req.params.id],
+		id: req.params.id
+	});
+});
+//put route
+router.put('/:id', (req, res) => {
+		Movies[req.params.id] = req.body;
+		res.redirect('/movies')
 })
 
 module.exports = router;
